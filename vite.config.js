c52e799f201154c8028d5178c6f36b205ec38ff9
Carol0127 <a52524444@gmail.com>
2025-08-08 20:02:02 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: "/WorkwayWeek1/",
+  base: "/workwayWeek2/",
   plugins: [
     liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
